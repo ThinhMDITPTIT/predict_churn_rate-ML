5926988526ba013ac8c1690cbf9531e0c6c3424a
ThinhMD <ThinhMDITPTIT@gmail.com>
2021-11-21 17:51:29 +0700

    try to replace func v.0.0.1 - ThinhMD

--- app.py
+++ app.py
@@ -9,45 +9,7 @@ import joblib
 model = joblib.load(r"./notebook/model.sav")
 
 #Import python scripts
-# from preprocessing import preprocess
-from sklearn.preprocessing import MinMaxScaler
-
-def preprocess(df, option):
-    """
-    This function is to cover all the preprocessing steps on the churn dataframe. It involves selecting important features, encoding categorical data, handling missing values,feature scaling and splitting the data
-    """
-    #Defining the map function
-    def binary_map(feature):
-        return feature.map({'Yes':1, 'No':0})
-
-    # Encode binary categorical features
-    binary_list = ['SeniorCitizen','Dependents', 'PhoneService', 'PaperlessBilling']
-    df[binary_list] = df[binary_list].apply(binary_map)
-
-    
-    #Drop values based on operational options
-    if (option == "Online"):
-        columns = ['SeniorCitizen', 'Dependents', 'tenure', 'PhoneService', 'PaperlessBilling', 'MonthlyCharges', 'TotalCharges', 'MultipleLines_No_phone_service', 'MultipleLines_Yes', 'InternetService_Fiber_optic', 'InternetService_No', 'OnlineSecurity_No_internet_service', 'OnlineSecurity_Yes', 'OnlineBackup_No_internet_service', 'TechSupport_No_internet_service', 'TechSupport_Yes', 'StreamingTV_No_internet_service', 'StreamingTV_Yes', 'StreamingMovies_No_internet_service', 'StreamingMovies_Yes', 'Contract_One_year', 'Contract_Two_year', 'PaymentMethod_Electronic_check']
-        #Encoding the other categorical categoric features with more than two categories
-        df = pd.get_dummies(df).reindex(columns=columns, fill_value=0)
-    elif (option == "Batch"):
-        pass
-        df = df[['SeniorCitizen','Dependents','tenure','PhoneService','MultipleLines','InternetService','OnlineSecurity',
-                'OnlineBackup','TechSupport','StreamingTV','StreamingMovies','Contract','PaperlessBilling','PaymentMethod',
-                'MonthlyCharges','TotalCharges']]
-        columns = ['SeniorCitizen', 'Dependents', 'tenure', 'PhoneService', 'PaperlessBilling', 'MonthlyCharges', 'TotalCharges', 'MultipleLines_No_phone_service', 'MultipleLines_Yes', 'InternetService_Fiber_optic', 'InternetService_No', 'OnlineSecurity_No_internet_service', 'OnlineSecurity_Yes', 'OnlineBackup_No_internet_service', 'TechSupport_No_internet_service', 'TechSupport_Yes', 'StreamingTV_No_internet_service', 'StreamingTV_Yes', 'StreamingMovies_No_internet_service', 'StreamingMovies_Yes', 'Contract_One_year', 'Contract_Two_year', 'PaymentMethod_Electronic_check']
-        #Encoding the other categorical categoric features with more than two categories
-        df = pd.get_dummies(df).reindex(columns=columns, fill_value=0)
-    else:
-        print("Incorrect operational options")
-
-
-    #feature scaling
-    sc = MinMaxScaler()
-    df['tenure'] = sc.fit_transform(df[['tenure']])
-    df['MonthlyCharges'] = sc.fit_transform(df[['MonthlyCharges']])
-    df['TotalCharges'] = sc.fit_transform(df[['TotalCharges']])
-    return df
+from preprocessing import preprocess
 
 def main():
     #Setting Application title
@@ -140,6 +102,7 @@ def main():
             st.markdown("<h3></h3>", unsafe_allow_html=True)
             #Preprocess inputs
             preprocess_df = preprocess(data, "Batch")
+            st.write(preprocess_df.head())
             if st.button('Predict'):
                 if len(preprocess_df) < 1:
                     st.warning('No file columns.')
