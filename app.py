5bb715aa315a45f174f0121a02c1102b8ad1fc9c
ThinhMD <ThinhMDITPTIT@gmail.com>
2021-11-29 19:07:16 +0700

    Optimize performance - ThinhMD

--- app.py
+++ app.py
@@ -1,124 +1,157 @@
-#Import libraries
-import streamlit as st
-import pandas as pd
-# import numpy as np
-# from PIL import Image
+import streamlit as stlit
+import pandas as pdas
 
-#load the model from disk
+# Load the predict churn model
 import joblib
+
 model = joblib.load(r"./notebook/model.sav")
 
-#Import python scripts
+# Import preprocessing scripts
 from preprocessing import preprocess
 
+
 def main():
-    #Setting Application title
-    st.title('Customer Churn Prediction App')
+    # Setting Application title
+    stlit.title("Customer Churn Prediction App")
 
-      #Setting Application description
-    # st.markdown("""
-    #  :dart:  This Streamlit app is made to predict customer churn in a ficitional telecommunication use case.
-    # The application is functional for both online prediction and batch data prediction. \n
-    # """)
-    # st.markdown("<h3></h3>", unsafe_allow_html=True)
+    # Application description
+    """
+    This Streamlit app is made to predict customer churn in a ficitional telecommunication use case.
+    The application is functional for both online prediction and batch data prediction.
+    """
 
-    #Setting Application sidebar default
-    # image = Image.open('App.jpg')
-    add_selectbox = st.sidebar.selectbox(
-	"How would you like to predict?", ("Online", "Batch"))
-    # st.sidebar.info('This app is created to predict Customer Churn')
-    # st.sidebar.image(image)
+    # Setting Application sidebar default
+    add_selectbox = stlit.sidebar.selectbox(
+        "How would you like to predict?", ("Online", "Batch")
+    )
 
     if add_selectbox == "Online":
-        # st.info("Input data below")
-        #Based on our optimal features selection
-        st.subheader("Demographic data")
-        seniorcitizen = st.selectbox('Senior Citizen:', ('Yes', 'No'))
-        dependents = st.selectbox('Dependent:', ('Yes', 'No'))
+        # Based on our optimal features selection
+        stlit.subheader("Demographic data")
+        seniorcitizen = stlit.selectbox("Senior Citizen:", ("Yes", "No"))
+        dependents = stlit.selectbox("Dependent:", ("Yes", "No"))
 
+        stlit.subheader("Payment data")
+        tenure = stlit.slider(
+            "Number of months the customer has stayed with the company",
+            min_value=0,
+            max_value=72,
+            value=0,
+        )
+        contract = stlit.selectbox(
+            "Contract", ("Month-to-month", "One year", "Two year")
+        )
+        paperlessbilling = stlit.selectbox("Paperless Billing", ("Yes", "No"))
+        PaymentMethod = stlit.selectbox(
+            "PaymentMethod",
+            (
+                "Electronic check",
+                "Mailed check",
+                "Bank transfer (automatic)",
+                "Credit card (automatic)",
+            ),
+        )
+        monthlycharges = stlit.number_input(
+            "The amount charged to the customer monthly",
+            min_value=0,
+            max_value=150,
+            value=0,
+        )
+        totalcharges = stlit.number_input(
+            "The total amount charged to the customer",
+            min_value=0,
+            max_value=10000,
+            value=0,
+        )
 
-        st.subheader("Payment data")
-        tenure = st.slider('Number of months the customer has stayed with the company', min_value=0, max_value=72, value=0)
-        contract = st.selectbox('Contract', ('Month-to-month', 'One year', 'Two year'))
-        paperlessbilling = st.selectbox('Paperless Billing', ('Yes', 'No'))
-        PaymentMethod = st.selectbox('PaymentMethod',('Electronic check', 'Mailed check', 'Bank transfer (automatic)','Credit card (automatic)'))
-        monthlycharges = st.number_input('The amount charged to the customer monthly', min_value=0, max_value=150, value=0)
-        totalcharges = st.number_input('The total amount charged to the customer',min_value=0, max_value=10000, value=0)
-
-        st.subheader("Services signed up for")
-        mutliplelines = st.selectbox("Does the customer have multiple lines",('Yes','No','No phone service'))
-        phoneservice = st.selectbox('Phone Service:', ('Yes', 'No'))
-        internetservice = st.selectbox("Does the customer have internet service", ('DSL', 'Fiber optic', 'No'))
-        onlinesecurity = st.selectbox("Does the customer have online security",('Yes','No','No internet service'))
-        onlinebackup = st.selectbox("Does the customer have online backup",('Yes','No','No internet service'))
-        techsupport = st.selectbox("Does the customer have technology support", ('Yes','No','No internet service'))
-        streamingtv = st.selectbox("Does the customer stream TV", ('Yes','No','No internet service'))
-        streamingmovies = st.selectbox("Does the customer stream movies", ('Yes','No','No internet service'))
+        stlit.subheader("Services signed up for")
+        mutliplelines = stlit.selectbox(
+            "Does the customer have multiple lines", ("Yes", "No", "No phone service")
+        )
+        phoneservice = stlit.selectbox("Phone Service:", ("Yes", "No"))
+        internetservice = stlit.selectbox(
+            "Does the customer have internet service", ("DSL", "Fiber optic", "No")
+        )
+        onlinesecurity = stlit.selectbox(
+            "Does the customer have online security",
+            ("Yes", "No", "No internet service"),
+        )
+        onlinebackup = stlit.selectbox(
+            "Does the customer have online backup", ("Yes", "No", "No internet service")
+        )
+        techsupport = stlit.selectbox(
+            "Does the customer have technology support",
+            ("Yes", "No", "No internet service"),
+        )
+        streamingtv = stlit.selectbox(
+            "Does the customer stream TV", ("Yes", "No", "No internet service")
+        )
+        streamingmovies = stlit.selectbox(
+            "Does the customer stream movies", ("Yes", "No", "No internet service")
+        )
 
         data = {
-                'SeniorCitizen': seniorcitizen,
-                'Dependents': dependents,
-                'tenure':tenure,
-                'PhoneService': phoneservice,
-                'MultipleLines': mutliplelines,
-                'InternetService': internetservice,
-                'OnlineSecurity': onlinesecurity,
-                'OnlineBackup': onlinebackup,
-                'TechSupport': techsupport,
-                'StreamingTV': streamingtv,
-                'StreamingMovies': streamingmovies,
-                'Contract': contract,
-                'PaperlessBilling': paperlessbilling,
-                'PaymentMethod':PaymentMethod, 
-                'MonthlyCharges': monthlycharges, 
-                'TotalCharges': totalcharges
-                }
-        features_df = pd.DataFrame.from_dict([data])
-        st.markdown("<h3></h3>", unsafe_allow_html=True)
-        st.write('Overview of input is shown below')
-        st.markdown("<h3></h3>", unsafe_allow_html=True)
-        st.dataframe(features_df)
+            "SeniorCitizen": seniorcitizen,
+            "Dependents": dependents,
+            "tenure": tenure,
+            "PhoneService": phoneservice,
+            "MultipleLines": mutliplelines,
+            "InternetService": internetservice,
+            "OnlineSecurity": onlinesecurity,
+            "OnlineBackup": onlinebackup,
+            "TechSupport": techsupport,
+            "StreamingTV": streamingtv,
+            "StreamingMovies": streamingmovies,
+            "Contract": contract,
+            "PaperlessBilling": paperlessbilling,
+            "PaymentMethod": PaymentMethod,
+            "MonthlyCharges": monthlycharges,
+            "TotalCharges": totalcharges,
+        }
+        features_df = pdas.DataFrame.from_dict([data])
+        stlit.markdown("<h3></h3>", unsafe_allow_html=True)
+        stlit.write("Overview of input is shown below")
+        stlit.markdown("<h3></h3>", unsafe_allow_html=True)
+        stlit.dataframe(features_df)
 
-
-        #Preprocess inputs
-        preprocess_df = preprocess(features_df, 'Online')
+        # Preprocess inputs
+        preprocess_df = preprocess(features_df, "Online")
 
         prediction = model.predict(preprocess_df)
 
-        if st.button('Predict'):
-            st.write(prediction)
+        if stlit.button("Predict"):
+            stlit.write(prediction)
             if prediction == 1:
-                st.warning('Yes, the customer will terminate the service.')
+                stlit.warning("Yes, the customer will terminate the service.")
             else:
-                st.success('No, the customer is happy with Telco Services.')
-        
+                stlit.success("No, the customer is happy with services.")
 
     else:
-        st.subheader("Dataset upload")
-        uploaded_file = st.file_uploader("Choose a file")
+        stlit.subheader("Dataset upload")
+        uploaded_file = stlit.file_uploader("Choose a file")
         if uploaded_file is not None:
-            data = pd.read_csv(uploaded_file)
+            data = pdas.read_csv(uploaded_file)
             uploaded_file.seek(0)
-            #Get overview of data
-            st.write(data.head())
-            st.markdown("<h3></h3>", unsafe_allow_html=True)
-            #Preprocess inputs
+            # Get overview of data
+            stlit.write(data.head())
+            stlit.markdown("<h3></h3>", unsafe_allow_html=True)
+            # Preprocess inputs
             preprocess_df = preprocess(data, "Batch")
-            if st.button('Predict'):
-                #Get batch prediction
+            if stlit.button("Predict"):
+                # Get batch prediction
                 prediction = model.predict(preprocess_df)
-                # st.write(prediction)
-                prediction_df = pd.DataFrame(prediction, columns=["Predictions"])
-                prediction_df = prediction_df.replace({1:'Yes, the customer will terminate the service.', 
-                                                    0:'No, the customer is happy with Telco Services.'})
+                prediction_df = pdas.DataFrame(prediction, columns=["Predictions"])
+                prediction_df = prediction_df.replace(
+                    {
+                        1: "Yes, the customer will terminate the service.",
+                        0: "No, the customer is happy with services.",
+                    }
+                )
 
-                st.markdown("<h3></h3>", unsafe_allow_html=True)
-                st.subheader('Prediction')
-                st.write(prediction_df)
-            
-if __name__ == '__main__':
-        main()
-
-
+                stlit.markdown("<h3></h3>", unsafe_allow_html=True)
+                stlit.subheader("Prediction")
+                stlit.write(prediction_df)
 
 
+if __name__ == "__main__":
+    main()
